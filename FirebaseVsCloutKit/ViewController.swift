2deb0c96015619ad73c22cc89424d8f91b8b54eb
JaydenGarrick <33009669+JaydenGarrick@users.noreply.github.com>
2018-09-19 09:56:26 -0600

    Initial Commit

---
+++ FirebaseVsCloutKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseVsCloutKit
+//
+//  Created by Jayden Garrick on 9/19/18.
+//  Copyright © 2018 Jayden Garrick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
